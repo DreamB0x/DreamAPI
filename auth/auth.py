e61e61515e2aeeb1c4e45823bc55e527c9e5a0bd
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-12-02 17:04:23 -0200

    get auth parameters using config

--- auth/auth.py
+++ auth/auth.py
@@ -4,6 +4,7 @@
 import json
 import requests
 
+from config.config import Config
 class Authentication(object):
 
     def __init__(self, uid, auth_code):
@@ -11,16 +12,19 @@ class Authentication(object):
         self.auth_code = auth_code
         self.authentication_url = "https://sb-autenticacao-api.original.com.br/OriginalConnect/AccessTokenController"
 
+    # Mounts dictionary for Original OAuth2 
     def mountOAuth2Data(self):
+        config = Config()
         oauth_data = {
             "uid": self.uid,
             "auth_code": self.auth_code,
-            "developer_key": "28f955c90b3a2940134ff1a970050f569a87facf",
-            "secret_key": "dd385cd0b59c013560400050569a7fac"
+            "developer_key": config.get("developer_key"),
+            "secret_key": config.get("secret_key")
         }
 
         return oauth_data
 
+    # Gets OAuth Token using
     def getOAuth2Token(self):
         data = json.dumps(self.mountOAuth2Data())
         ori_response = requests.post(self.authentication_url, data=data)
